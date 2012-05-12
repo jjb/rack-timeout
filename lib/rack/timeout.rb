a8d0321f2bfe94e6d8b424d33e9d1af27ad8bd7b
John Joseph Bachir <j@jjb.cc>
2012-05-11 22:36:04 -0400

    send 504 and 10-second refresh on timeout
    Instead of sending an exception up the call stack,
     * send a 504
     * have the user's browser refresh in 10 seconds
     * show the user an adorable fail whale

--- lib/rack/timeout.rb
+++ lib/rack/timeout.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require RUBY_VERSION < '1.9' ? 'system_timer' : 'timeout'
 SystemTimer ||= Timeout
 
@@ -13,9 +14,30 @@ module Rack
     end
 
     def call(env)
-      SystemTimer.timeout(self.class.timeout, Rack::Timeout::AppTimeout) {
-        @app.call(env)
-      }
+      begin
+        SystemTimer.timeout(self.class.timeout, Rack::Timeout::AppTimeout) {
+          @app.call(env)
+        }
+      rescue Rack::Timeout::AppTimeout
+        [ 504, {
+          "Refresh" => "10",
+          "Content-Type" => "text/plain; charset=utf-8"
+        }, ["
+▄██████████████▄▐█▄▄▄▄█▌
+██████▌▄▌▄▐▐▌███▌▀▀██▀▀
+████▄█▌▄▌▄▐▐▌▀███▄▄█▌
+▄▄▄▄▄██████████████▀
+
+Our systems are over
+capacity and could not
+complete your requeset.
+
+Your browser will
+automatically try again
+in 10 seconds.
+"
+        ]]
+      end
     end
 
     class AppTimeout < ::Timeout::Error; end
